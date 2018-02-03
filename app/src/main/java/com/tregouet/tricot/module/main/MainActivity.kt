2676d806563034eaafb56e58c3f829dc908d0b16
Marie-Cécile Trégouët <mc.tregouet@telecom-bretagne.eu>
2018-02-03 13:43:28 +0100

    [Menu] Add menu to Main Activity

--- app/src/main/java/com/tregouet/tricot/module/main/MainActivity.kt
+++ app/src/main/java/com/tregouet/tricot/module/main/MainActivity.kt
@@ -4,8 +4,9 @@ import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.support.v4.view.GravityCompat
 import android.support.v7.widget.LinearLayoutManager
+import com.balysv.materialmenu.MaterialMenuDrawable
 import com.tregouet.tricot.R
 import com.tregouet.tricot.model.Project
 import com.tregouet.tricot.module.base.BaseActivity
@@ -14,11 +15,17 @@ import com.tregouet.tricot.utils.RealmManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.popup_add_project.*
+import kotlinx.android.synthetic.main.top_bar.*
+import android.support.v4.widget.DrawerLayout
+import android.view.View
+import kotlinx.android.synthetic.main.menu.*
+
 
 class MainActivity : BaseActivity() {
 
     private var adapter : ProjectsAdapter?=null
-    private var projects : ArrayList<Project> = ArrayList<Project>()
+    private var projects : ArrayList<Project> = ArrayList()
+    private var materialMenu: MaterialMenuDrawable? = null
 
     /**
      * onCreate
@@ -26,6 +33,37 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        materialMenu = MaterialMenuDrawable(this, Color.WHITE, MaterialMenuDrawable.Stroke.THIN)
+        burger.setImageDrawable(materialMenu)
+        burger.setOnClickListener {
+            if (drawer_layout.isDrawerOpen(GravityCompat.START)){
+                materialMenu!!.animateIconState(MaterialMenuDrawable.IconState.BURGER)
+                drawer_layout.closeDrawers()
+            } else {
+                materialMenu!!.animateIconState(MaterialMenuDrawable.IconState.ARROW)
+                drawer_layout.openDrawer(GravityCompat.START)
+            }
+        }
+
+        drawer_layout.addDrawerListener(object : DrawerLayout.SimpleDrawerListener() {
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                materialMenu?.setTransformationOffset(
+                        MaterialMenuDrawable.AnimationState.BURGER_ARROW,
+                        if (drawer_layout.isDrawerOpen(GravityCompat.START)) 2 - slideOffset else slideOffset
+                )
+            }
+
+            override fun onDrawerStateChanged(newState: Int) {
+                if(newState == DrawerLayout.STATE_IDLE) {
+                    if (drawer_layout.isDrawerOpen(GravityCompat.START)){
+                        materialMenu?.iconState = MaterialMenuDrawable.IconState.ARROW
+                    } else {
+                        materialMenu?.iconState = MaterialMenuDrawable.IconState.BURGER
+                    }
+                }
+            }
+        })
 
         fab.setOnClickListener { _ ->
             val dialog = Dialog(this)
@@ -41,6 +79,11 @@ class MainActivity : BaseActivity() {
             }
             dialog.show()
         }
+
+        how_it_works.setOnClickListener { showHowItWorks() }
+        my_stats.setOnClickListener { showMyStats() }
+        infos.setOnClickListener { showInfos() }
+
     }
 
     /**
@@ -63,5 +106,26 @@ class MainActivity : BaseActivity() {
         projects_recyclerview.layoutManager = LinearLayoutManager(this)
         adapter = ProjectsAdapter(this, projects)
         projects_recyclerview.adapter = adapter
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)){
+            drawer_layout.closeDrawers()
+            return
+        }
+
+        super.onBackPressed()
+    }
+
+    private fun showHowItWorks() {
+        //TODO show how it works
+    }
+
+    private fun showMyStats() {
+        //TODO show my stats
+    }
+
+    private fun showInfos() {
+        //TODO show infos
     }
 }
